4a65b971e8dcbfb62299434e98df47c711d4c63a
CoderMe <srcii@coderme.com>
2018-09-06 15:57:32 +0300

    _

--- src/qxn/jinja2.py
+++ src/qxn/jinja2.py
@@ -1,6 +1,8 @@
+from jinja2 import Environment
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
-from jinja2 import Environment
+from django.utils import timezone
+
 
 
 
@@ -10,6 +12,7 @@ def environment(**options):
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
+        'now': timezone.now(),
     })
     # more options if DEBUG is disabled
     # options['auto_reload'] = settings.DEBUG
