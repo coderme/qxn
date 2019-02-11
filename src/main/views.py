7148c48d7eb1196d2c6ce1e8db76ebc692d0b8e5
CoderMe <srcii@coderme.com>
2019-02-11 15:17:39 +0200

    versin 0.1a2
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
+from .models import Question
+
 
 class QuestionDetail(DetailView):
+    model = Question
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
